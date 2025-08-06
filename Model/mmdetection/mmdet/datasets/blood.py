7267881e57ec9053be49e342e81ace39c6e8c16d
nakyung1007 <whskrud1007@naver.com>
2025-08-06 18:15:13 +0900

    [setting]#5 - blood.py 데이터셋 세팅

--- Model/mmdetection/mmdet/datasets/blood.py
+++ Model/mmdetection/mmdet/datasets/blood.py
@@ -6,6 +6,7 @@ from typing import List, Union
 from mmengine.fileio import get_local_path
 
 from mmdet.registry import DATASETS
+#from mmdet.datasets.custom import CustomDataset
 from .api_wrappers import COCO
 from .base_det_dataset import BaseDetDataset
 
